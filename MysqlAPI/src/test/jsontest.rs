3a7a5619ab9a8112053dd7c9d15dd26cabe946d8
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-11 17:19:49 +0800

    1
    1

--- MysqlAPI/src/test/jsontest.rs
+++ MysqlAPI/src/test/jsontest.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct MyStruct {
+struct UserDemo {
     name: String,
     age: u32,
 }
 pub fn testjson() {
     // Create an instance of the struct
-    let person = MyStruct {
+    let person = UserDemo {
         name: String::from("Alice"),
         age: 30,
     };
@@ -17,6 +17,6 @@ pub fn testjson() {
     println!("Serialized: {}", serialized);
 
     // Deserialize the JSON string back to a struct
-    let deserialized: MyStruct = serde_json::from_str(&serialized).unwrap();
+    let deserialized: UserDemo = serde_json::from_str(&serialized).unwrap();
     println!("Deserialized: {:?}", deserialized);
 }
