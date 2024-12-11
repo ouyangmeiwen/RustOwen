435ff5d0c8c4bcca56931141c895aea252efeb2e
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-11 14:45:42 +0800

    12
    123

--- MysqlAPI/src/handlers/auth_handler.rs
+++ MysqlAPI/src/handlers/auth_handler.rs
@@ -28,7 +28,10 @@ pub async fn generate_token_handler(
                 Ok(_) => (),
                 Err(e) => (),
             }
-            // RedisClient::close_connection(&redis_client);
+            redis_client
+                .expire(&user_id, (24 * 3600) as i64)
+                .await
+                .unwrap_or(());
             return HttpResponse::Ok().json(TokenResponse { token });
         }
         Err(_) => HttpResponse::InternalServerError().json(json!({
@@ -46,12 +49,25 @@ pub async fn generate_token_get_handler(
     let user_id = body.user_id.clone(); // 从请求体中提取 user_id
 
     let my_claims = Claims {
-        user_id: user_id,
+        user_id: user_id.clone(),
         exp: (Utc::now() + chrono::Duration::hours(24)).timestamp() as usize,
         ..Default::default()
     };
     match create_jwt(&my_claims) {
-        Ok(token) => HttpResponse::Ok().json(TokenResponse { token }),
+        Ok(token) => {
+            // 初始化 Redis 客户端
+            let redis_client = RedisClient::default().await.unwrap();
+            // 确保 `set` 方法返回的是 `Result<(), String>` 类型，否则你需要做额外的错误处理
+            match redis_client.set(&user_id, &token).await {
+                Ok(_) => (),
+                Err(e) => (),
+            }
+            redis_client
+                .expire(&user_id, (24 * 3600) as i64)
+                .await
+                .unwrap_or(());
+            return HttpResponse::Ok().json(TokenResponse { token });
+        }
         Err(_) => HttpResponse::InternalServerError().json(json!({
             "status": "error",
             "message": "Failed to generate token"
