d967b980af7406af96cecda64b29318ad5d5ad8b
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-13 17:59:19 +0800

    1
    1

--- MysqlAPI/src/models/static_model.rs
+++ MysqlAPI/src/models/static_model.rs
@@ -2,5 +2,6 @@ use lazy_static::lazy_static;
 use std::sync::Mutex;
 lazy_static! {
    // 使用 Mutex 包裹 String，这样可以在需要时修改其值
-    pub static ref RABBITMQ_ROUTING_EXCHANGE: Mutex<String> = Mutex::new(String::new());
+    pub static ref RABBITMQ_ROUTING_EXCHANGE: Mutex<String> = Mutex::new(String::new());// 使用 Mutex 包裹 i32，这样可以在需要时修改其值
+    pub static ref GLOBAL_NUMBER: Mutex<i32> = Mutex::new(0);
 }
