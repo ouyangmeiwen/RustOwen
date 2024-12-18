cc24911e37521cf129a364009bdfac6acd1fcaf5
追梦的人 <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-18 21:52:33 +0800

    1
    1

--- MysqlAPI/src/configs/envconfig.rs
+++ MysqlAPI/src/configs/envconfig.rs
@@ -5,85 +5,55 @@ use serde_yaml;
 use std::env;
 use std::fs;
 use std::string::String;
-use std::sync::RwLock; // Ensure to add serde_yaml as a dependency in Cargo.toml
+use std::sync::RwLock;
+
 impl Config {
-    pub fn new_yarm() -> Config {
-        // Read the file contents as raw bytes (Vec<u8>)
-        let contents: Vec<u8> = fs::read("config.yaml").expect("Failed to read file");
-        // Convert the bytes into a UTF-8 string
+    // 辅助函数：从环境变量中获取并解析值
+    fn get_env_var<T: std::str::FromStr>(key: &str, default: Option<T>) -> T {
+        match env::var(key) {
+            Ok(value) => value
+                .parse::<T>()
+                .unwrap_or_else(|_| panic!("Failed to parse {} from environment", key)),
+            Err(_) => default.unwrap_or_else(|| panic!("{} must be set in .env file", key)),
+        }
+    }
+
+    pub fn new_yarn() -> Config {
+        // 读取 YAML 配置文件并解析
+        let contents = fs::read("config.yaml").expect("Failed to read config.yaml");
         let string_contents =
             String::from_utf8(contents).expect("Failed to convert bytes to string");
-        // Parse the string contents (YAML) into a Config struct
         let config: Config = serde_yaml::from_str(&string_contents).expect("Failed to parse YAML");
         config
     }
+
     pub fn new() -> Config {
         dotenv().ok(); // 加载 .env 文件
 
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env file");
-
-        let port = env::var("PORT")
-            .expect("PORT must be set in .env file")
-            .parse()
-            .expect("PORT must be a valid u16");
-
-        let cors_allowed_origin =
-            env::var("CORS_ALLOWED_ORIGIN").expect("CORS_ALLOWED_ORIGIN must be set in .env file");
-
-        let max_connections = env::var("MAX_CONNECTIONS")
-            .expect("MAX_CONNECTIONS must be set in .env file")
-            .parse()
-            .expect("MAX_CONNECTIONS must be a valid u32");
-
-        let log_level = env::var("LOG_LEVEL").expect("LOG_LEVEL must be set in .env file");
-        let secret_key = env::var("SECRET_KEY").expect("SECRET_KEY must be set in .env file");
-        let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set in .env file");
-
-        let rabbitmq_uri = env::var("RABBITMQ_URI").expect("RABBITMQ_URI must be set in .env file");
-        let rabbitmq_exchange =
-            env::var("RABBITMQ_EXCHANGE").expect("RABBITMQ_EXCHANGE must be set in .env file");
-        let rabbitmq_queue =
-            env::var("RABBITMQ_QUEUE").expect("RABBITMQ_QUEUE must be set in .env file");
-        let rabbitmq_routing_key_send = env::var("RABBITMQ_ROUTING_KEY_SEND")
-            .expect("RABBITMQ_ROUTING_KEY_SEND must be set in .env file");
-        let rabbitmq_routing_key_revceived = env::var("RABBITMQ_ROUTING_KEY_RECEIVED")
-            .expect("RABBITMQ_ROUTING_KEY_RECEIVED must be set in .env file");
-
-        let limit_per_second_default = env::var("LIMIT_PER_SECOND_DEFAULT")
-            .expect("LIMIT_PER_SECOND_DEFAULT must be set in .env file")
-            .parse()
-            .expect("LIMIT_PER_SECOND_DEFAULT must be a valid u64");
-
-        let time_window_secs_default = env::var("TIME_WINDOW_SECS_DEFAULT")
-            .expect("TIME_WINDOW_SECS_DEFAULT must be set in .env file")
-            .parse()
-            .expect("TIME_WINDOW_SECS_DEFAULT must be a valid u64");
-
-        let limit_ip = env::var("LIMIT_IP")
-            .expect("LIMIT_IP must be set in .env file")
-            .parse()
-            .expect("LIMIT_IP must be a valid u64");
-
         Config {
-            database_url,
-            port,
-            cors_allowed_origin,
-            max_connections,
-            log_level,
-            secret_key,
-            redis_url,
-            rabbitmq_uri,
-            rabbitmq_exchange,
-            rabbitmq_queue,
-            rabbitmq_routing_key_send,
-            rabbitmq_routing_key_revceived,
-            limit_per_second_default,
-            time_window_secs_default,
-            limit_ip,
+            database_url: Config::get_env_var("DATABASE_URL", None),
+            port: Config::get_env_var("PORT", None),
+            cors_allowed_origin: Config::get_env_var("CORS_ALLOWED_ORIGIN", None),
+            max_connections: Config::get_env_var("MAX_CONNECTIONS", None),
+            log_level: Config::get_env_var("LOG_LEVEL", None),
+            secret_key: Config::get_env_var("SECRET_KEY", None),
+            redis_url: Config::get_env_var("REDIS_URL", None),
+            rabbitmq_uri: Config::get_env_var("RABBITMQ_URI", None),
+            rabbitmq_exchange: Config::get_env_var("RABBITMQ_EXCHANGE", None),
+            rabbitmq_queue: Config::get_env_var("RABBITMQ_QUEUE", None),
+            rabbitmq_routing_key_send: Config::get_env_var("RABBITMQ_ROUTING_KEY_SEND", None),
+            rabbitmq_routing_key_revceived: Config::get_env_var(
+                "RABBITMQ_ROUTING_KEY_RECEIVED",
+                None,
+            ),
+            limit_per_second_default: Config::get_env_var("LIMIT_PER_SECOND_DEFAULT", None),
+            time_window_secs_default: Config::get_env_var("TIME_WINDOW_SECS_DEFAULT", None),
+            limit_ip: Config::get_env_var("LIMIT_IP", None),
         }
     }
 }
+
 lazy_static! {
-    // 使用 Mutex 包裹 Config，确保线程安全
+    // 使用 RwLock 包裹 Config，确保线程安全
     pub static ref STATIC_CONFIG: RwLock<Config> = RwLock::new(Config::new());
 }
