f33ae49cd4b829546bdf8efcf664abbac0e866e3
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-20 10:04:16 +0800

    1
    1

--- MysqlAPI/src/models/rabbitmq_model.rs
+++ MysqlAPI/src/models/rabbitmq_model.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use tokio::sync::mpsc;
 #[derive(Clone)] // 自动为 RabbitMQ 实现 Clone
 pub struct RabbitMQ {
-    pub connection: Arc<Connection>,
-    pub channel: Arc<Channel>,
+    pub connection: Arc<Connection>,        //保证 线性安全
+    pub channel: Arc<Channel>,              //线性安全
     pub exchange_declared: Arc<AtomicBool>, // 新增字段
 }
